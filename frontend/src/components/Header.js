cc85dd7906bb40f33fe61f71fe8bfe689a3168b7
aryamanish1234 <manishiit900@hmail.com>
2021-05-19 22:33:25 +0530

    first commit

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Header() {
+return(
+    <nav className="navbar navbar-light bg-primary">
+  <div className="container-fluid">
+    <div className="h3 text-center text-light w-100">Grocery App</div>
+  </div>
+</nav>
+);
+}
